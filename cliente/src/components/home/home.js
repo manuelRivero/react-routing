c09d3b48a268be6d0ed3c4420c50d2019410e49c
manuel rivero <manuel.enrique.r.v@gmail.com>
2019-12-04 23:56:39 -0300

    [CSS]: Improve color and font size to the home hero

--- cliente/src/components/home/home.js
+++ cliente/src/components/home/home.js
@@ -5,11 +5,12 @@ export default function Home() {
   return (
     <>
         <div className={[styles.Home, "section-container"].join(" ")}>
+                <div className={styles.Gradient}></div>
                 <div className={styles.Card}>
                     <h1>Lo mas exclusivo a menor precio!</h1>
                     <p>Ingresa para obtener los mejores descuentos y premios asombrosos.</p>
                     <button>Log in</button>
-                    <button>Empezar a comprar!</button>
+                    <button className={styles.StartBtn}>Empezar a comprar!</button>
                 </div>
         </div>  
     </>
