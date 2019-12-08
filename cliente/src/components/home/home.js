7653823953e407c0ed3b79b7a00ff73e18cd5cf9
manuel rivero <manuel.enrique.r.v@gmail.com>
2019-12-08 00:55:26 -0300

    [Css]: add fade in animation to home component

--- cliente/src/components/home/home.js
+++ cliente/src/components/home/home.js
@@ -1,18 +1,19 @@
-import React from 'react';
-import styles from './home.module.css'
+import React from 'react'
+import styles from './home.module.css';
+import {Link} from 'react-router-dom'
 
 export default function Home() {
-  return (
-    <>
-        <div className={[styles.Home, "section-container"].join(" ")}>
-                <div className={styles.Gradient}></div>
-                <div className={styles.Card}>
-                    <h1>Lo mas exclusivo a menor precio!</h1>
-                    <p>Ingresa para obtener los mejores descuentos y premios asombrosos.</p>
-                    <button>Log in</button>
-                    <button className={styles.StartBtn}>Empezar a comprar!</button>
+  // hacer redireccionamiento automatico a los productos si el usuario ya esta logueado
+  return ( 
+  <div className={[styles.Home, "section-container"].join(" ")}>
+                <div className={[styles.Card, "fadeUp"].join(" ")}>
+                    <div className="fadeDown" style={{marginBottom: "5rem"}}>
+                      <h1>Lo mas exclusivo a menor precio!</h1>
+                      <p>Ingresa para obtener los mejores descuentos y premios asombrosos.</p>
+                    </div>
+                    <button className={styles.Button}>Log in</button>
+                    <Link className={[styles.StartBtn, styles.Button].join(" ")} to="/productos">Empezar a comprar!</Link>
                 </div>
         </div>  
-    </>
   );
 }
