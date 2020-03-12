d0b83fb1a74012c0e41c83cdfad7b0a5bba6347e
manuel rivero <manuel.enrique.r.v@gmail.com>
2020-03-11 22:20:36 -0300

    Updates

---
+++ precache-manifest.a85c3debef08d8aea3e8cd56c6af51b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c5590569aab553af03722baa7d379978",
+    "url": "/react-routing/static/media/loading.c5590569.svg"
+  },
+  {
+    "revision": "3a657f46235fb0416efe",
+    "url": "/react-routing/static/js/runtime~main.51ad13a7.js"
+  },
+  {
+    "revision": "89d492838b4b8cc0688e",
+    "url": "/react-routing/static/js/main.1614638c.chunk.js"
+  },
+  {
+    "revision": "d8da6301676662a5ebb3",
+    "url": "/react-routing/static/js/2.0ab68946.chunk.js"
+  },
+  {
+    "revision": "89d492838b4b8cc0688e",
+    "url": "/react-routing/static/css/main.5c7e5d40.chunk.css"
+  },
+  {
+    "revision": "204d452feaf0b09b163a95d6b1b9d573",
+    "url": "/react-routing/index.html"
+  }
+];
